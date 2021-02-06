936f3c388e5fdaaee5fb3e9e08be0a7e063bf342
Vitaliy Belykaov <66310947+Vitaliy-Bel@users.noreply.github.com>
2021-02-06 09:32:10 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
-alert ('Привет');
+
 //  Получение случайного целого числа в заданном интервале, включительно
-const getRandomIntInclusive = function (min, max) {
-  if (max>min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+let getRandomIntInclusive = function (min, max) {
+
+  if (max > min) {
+    //min = Math.ceil(min);
+    //max = Math.floor(max);
+    return getRandomIntInclusive = Math.floor(Math.random() * (max - min + 1)) + min; //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   }
   else {
-    console.log('Error');  // Я так понимаю console.log нельзя? и не понимаю что здесь надо
+    let difference = max - min;
+    difference = Math.abs(difference);
+    return getRandomIntInclusive = Math.floor(Math.random() * (difference + 1)) + min;
+    console.log (difference);
   }
 }
-//  Получение случайного числа в заданном интервале, включительно
-getRandomIntInclusive(20, 10);
+
+console.log(getRandomIntInclusive(20, 10));
 
 
 
 let getRandomIntInclusiveFloatingPoint = function (min, max, numberofdecimalplaces) {
-  if (max>min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let randNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    return getRandomIntInclusiveFloatingPoint = parseFloat(randNum.toFixed(numberofdecimalplaces)); // Почему здесь ошибка если вверху объявляешь const getRandomIntInclusiveFloatingPoint?
-  }
+  if (max > min) {
+    let randNum = Math.random() * (max - min + 1) + min;  // почему не округляет до целого если так: randNum = Math.floor(Math.random() * (max - min + 1)) + min
+    return getRandomIntInclusiveFloatingPoint = randNum.toFixed(numberofdecimalplaces); // Почему здесь ошибка если вверху объявляешь const getRandomIntInclusiveFloatingPoint?
+  }                                                                                                    // И почему если обернуть randNum.toFixed(numberofdecimalplaces) в parseFloat, то целое число получается?
   else {
-    console.log('Error');  // Я так понимаю console.log нельзя? и не понимаю что здесь надо
+    let difference = max - min;
+    difference = Math.abs(difference);
+    return getRandomIntInclusiveFloatingPoin = Math.floor(Math.random() * (difference + 1)) + min;
+
   }
 }
+console.log(getRandomIntInclusiveFloatingPoint(10.154, 20.234, 3));
 
-getRandomIntInclusiveFloatingPoint(20, 10, 4);
